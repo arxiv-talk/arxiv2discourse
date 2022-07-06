4614a54035b736aebb8172f235ef25d8be27a4f2
Matteo Smerlak <matteo.smerlak@gmail.com>
2022-07-06 10:42:53 +0200

    category names

--- create-categories.py
+++ create-categories.py
@@ -1,5 +1,3 @@
-
-
 # Discourse API
 API_KEY = 'e3d20e29465a164c40b065d37ba1a24d920ae59303ab67ae48e479fe6b116019'
 
@@ -15,26 +13,26 @@ palette = sns.color_palette("Spectral", 20).as_hex()
 colors = [color.removeprefix('#') for color in palette]
 
 categories = [
-    {'name': 'A - Computer Science', 'color': colors[0]},
-    {'name': 'A - Economics', 'color': colors[1]},
-    {'name': 'A - Electrical Engineering and Systems Science','color': colors[2]},
-    {'name': 'A - Mathematics','color': colors[3]},
-    {'name': 'A - Astrophysics','color': colors[4]},
-    {'name': 'A - Condensed Matter','color': colors[5]},
-    {'name': 'A - General Relativity and Quantum Cosmology', 'color': colors[6]},
-    {'name': 'A - High Energy Physics - Experiment', 'color': colors[7]},
-    {'name': 'A - High Energy Physics - Lattice', 'color': colors[8]},
-    {'name': 'A - High Energy Physics - Phenomenology', 'color': colors[9]},
-    {'name': 'A - High Energy Physics - Theory', 'color': colors[10]},
-    {'name': 'A - Mathematical Physics', 'color': colors[11]},
-    {'name': 'A - Nonlinear Sciences', 'color': colors[12]},
-    {'name': 'A - Nuclear Experiment', 'color': colors[13]},
-    {'name': 'A - Nuclear Theory', 'color': colors[14]},
-    {'name': 'A - Physics', 'color': colors[15]},
-    {'name': 'A - Quantum Physics', 'color': colors[16]},
-    {'name': 'A - Quantitative Biology', 'color': colors[17]},
-    {'name': 'A - Quantitative Finance', 'color': colors[18]},
-    {'name': 'A - Statistics', 'color': colors[19]}
+    {'name': 'Computer Science', 'color': colors[0]},
+    {'name': 'Economics', 'color': colors[1]},
+    {'name': 'Electrical Engineering and Systems Science','color': colors[2]},
+    {'name': 'Mathematics','color': colors[3]},
+    {'name': 'Astrophysics','color': colors[4]},
+    {'name': 'Condensed Matter','color': colors[5]},
+    {'name': 'General Relativity and Quantum Cosmology', 'color': colors[6]},
+    {'name': 'High Energy Physics - Experiment', 'color': colors[7]},
+    {'name': 'High Energy Physics - Lattice', 'color': colors[8]},
+    {'name': 'High Energy Physics - Phenomenology', 'color': colors[9]},
+    {'name': 'High Energy Physics - Theory', 'color': colors[10]},
+    {'name': 'Mathematical Physics', 'color': colors[11]},
+    {'name': 'Nonlinear Sciences', 'color': colors[12]},
+    {'name': 'Nuclear Experiment', 'color': colors[13]},
+    {'name': 'Nuclear Theory', 'color': colors[14]},
+    {'name': 'Physics', 'color': colors[15]},
+    {'name': 'Quantum Physics', 'color': colors[16]},
+    {'name': 'Quantitative Biology', 'color': colors[17]},
+    {'name': 'Quantitative Finance', 'color': colors[18]},
+    {'name': 'Statistics', 'color': colors[19]}
 ]
 
 symbols = ['cs', 'econ', 'eess', 'math', 'astro-ph', 'cond-mat', 'gr-qc', 'hep-ex', 'hep-lat', 'hep-ph', 'hep-th', 'math-ph', 'nlin', 'nucl-ex', 'nucl-th', 'physics', 'quant-ph', 'q-bio', 'q-fin', 'stat']
